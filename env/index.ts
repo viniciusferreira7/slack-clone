d56edbf4566a49d712e3cba6b277ba31210075a4
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-01-09 08:02:22 -0300

    feat: adds authentication with google

--- env/index.ts
+++ env/index.ts
@@ -4,12 +4,21 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     CONVEX_DEPLOYMENT: z.string(),
+    AUTH_GITHUB_ID: z.string(),
+    AUTH_GITHUB_SECRET: z.string(),
+    AUTH_GOOGLE_ID: z.string(),
+    AUTH_GOOGLE_SECRET: z.string(),
   },
   client: {
     NEXT_PUBLIC_CONVEX_URL: z.string().url(),
   },
   runtimeEnv: {
     CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
   },
 })
