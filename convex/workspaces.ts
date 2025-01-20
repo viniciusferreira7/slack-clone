6781f5767d8edfd106338a840160a4c26d393505
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-01-20 08:44:35 -0300

    feat: adds method to create a workspace in database

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,8 +1,32 @@
-import { query } from './_generated/server'
+import { getAuthUserId } from '@convex-dev/auth/server'
+import { v } from 'convex/values'
+
+import { mutation, query } from './_generated/server'
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return ctx.db.query('workspaces').collect()
+  },
+})
+
+export const create = mutation({
+  args: { workspace_name: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx)
+    if (userId === null) {
+      return new Error('Unauthorized')
+    }
+
+    // TODO: Create proper method later.
+    const joinCode = '123456'
+
+    const newWorkspaceId = await ctx.db.insert('workspaces', {
+      name: args.workspace_name,
+      userId,
+      joinCode,
+    })
+
+    return { workspaceId: newWorkspaceId }
   },
 })
