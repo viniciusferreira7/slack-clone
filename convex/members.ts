1c843175fa06b77890b24e780edd2d2f0d6bdf0a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-03-03 10:29:06 -0300

    feat: create query to get members

--- convex/members.ts
+++ convex/members.ts
@@ -1,7 +1,55 @@
 import { getAuthUserId } from '@convex-dev/auth/server'
 import { v } from 'convex/values'
 
-import { query } from './_generated/server'
+import type { Id } from './_generated/dataModel'
+import { query, type QueryCtx } from './_generated/server'
+
+const getUserById = async (ctx: QueryCtx, userId: Id<'users'>) => {
+  return await ctx.db.get(userId)
+}
+
+export const get = query({
+  args: {
+    workspaceId: v.id('workspaces'),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx)
+
+    if (!userId) {
+      return null
+    }
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.workspaceId).eq('userId', userId),
+      )
+      .unique()
+
+    if (!member) {
+      return []
+    }
+
+    const data = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.workspaceId),
+      )
+      .collect()
+
+    const members = await Promise.all(
+      data.map(async (item) => {
+        const user = await getUserById(ctx, item.userId)
+        return {
+          ...member,
+          user,
+        }
+      }),
+    )
+
+    return members.filter((item) => !!item?._id)
+  },
+})
 
 export const currentMember = query({
   args: {
