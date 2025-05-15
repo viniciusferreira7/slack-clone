cb272501b12400102c7731a00b88b10b356966c8
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-15 09:45:18 -0300

    feat: add custom hook to update message

---
+++ src/features/messages/api/use-update-message.ts
@@ -0,0 +1,70 @@
+'use client'
+
+import { useMutation } from 'convex/react'
+import { useCallback, useMemo, useState } from 'react'
+
+import { api } from '../../../../convex/_generated/api'
+import type { Id } from '../../../../convex/_generated/dataModel'
+
+interface UpdateMessageRequest {
+  id: Id<'messages'>
+  body: string
+}
+interface UpdateMessageResponse {
+  messageId: Id<'messages'>
+}
+
+interface Options {
+  onSuccess?: (data: UpdateMessageResponse) => void
+  onError?: (err: unknown) => void
+  onSettled?: VoidFunction
+  throwError?: boolean
+}
+
+type Status = 'pending' | 'error' | 'success' | 'settled' | null
+
+export const useUpdateMessage = () => {
+  const [data, setData] = useState<UpdateMessageResponse | null>(null)
+  const [error, setError] = useState<null | unknown>()
+
+  const [status, setStatus] = useState<Status>(null)
+
+  const isPending = useMemo(() => status === 'pending', [status])
+  const isError = useMemo(() => status === 'error', [status])
+  const isSuccess = useMemo(() => status === 'success', [status])
+  const isSettled = useMemo(() => status === 'settled', [status])
+
+  const mutation = useMutation(api.messages.update)
+
+  const mutate = useCallback(
+    async (values: UpdateMessageRequest, options?: Options) => {
+      try {
+        setData(null)
+
+        setStatus('pending')
+
+        const response = await mutation(values)
+
+        if (response instanceof Error) return
+
+        options?.onSuccess?.(response)
+        setData(response)
+        setStatus('success')
+      } catch (err) {
+        setStatus('error')
+        options?.onError?.(err)
+        setError(err)
+
+        if (options?.throwError) {
+          throw err
+        }
+      } finally {
+        options?.onSettled?.()
+        setStatus('settled')
+      }
+    },
+    [mutation],
+  )
+
+  return { mutate, data, error, isPending, isError, isSuccess, isSettled }
+}
