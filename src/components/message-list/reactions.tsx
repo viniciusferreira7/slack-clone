ca425a4abfc2184efcc6edb5058da6d050ed9c33
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-22 09:30:44 -0300

    feat: add feature to add new reaction in reactions component

--- src/components/message-list/reactions.tsx
+++ src/components/message-list/reactions.tsx
@@ -1,8 +1,12 @@
+import { SmilePlus } from 'lucide-react'
+
 import { useCurrentMember } from '@/features/members/api/use-current-member'
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
 import { cn } from '@/lib/utils'
 
 import { Doc, type Id } from '../../../convex/_generated/dataModel'
+import { EmojiPopover } from '../emoji-popover'
+import { Hint } from '../hint'
 
 interface ReactionsProps {
   reactions: Array<
@@ -29,29 +33,41 @@ export function Reactions({ reactions, onReactionChange }: ReactionsProps) {
     <div className="mb-1 mt-1 flex items-center gap-1">
       {reactions.map((reaction) => {
         return (
-          <button
+          <Hint
             key={reaction._id}
-            onClick={() => onReactionChange(reaction.value)}
-            className={cn(
-              'flex h-6 items-center gap-x-1 rounded-full border border-transparent bg-slate-200/70 px-2 text-slate-800',
-              reaction.memberIds.includes(currentMember._id) &&
-                'border-blue-500 bg-blue-100/70 text-blue-500',
-            )}
+            label={`${reaction.count} ${reaction.count === 1 ? 'person' : 'people'} reacted with ${reaction.value}`}
           >
-            {reaction.value}
-            <span
-              className={
-                cn(
-                  'text-xs font-semibold text-muted-foreground',
-                  reaction.memberIds.includes(currentMember._id),
-                ) && 'text-blue-500'
-              }
+            <button
+              onClick={() => onReactionChange(reaction.value)}
+              className={cn(
+                'flex h-6 items-center gap-x-1 rounded-full border border-transparent bg-slate-200/70 px-2 text-slate-800',
+                reaction.memberIds.includes(currentMember._id) &&
+                  'border-blue-500 bg-blue-100/70 text-blue-500',
+              )}
             >
-              {reaction.count}
-            </span>
-          </button>
+              {reaction.value}
+              <span
+                className={
+                  cn(
+                    'text-xs font-semibold text-muted-foreground',
+                    reaction.memberIds.includes(currentMember._id),
+                  ) && 'text-blue-500'
+                }
+              >
+                {reaction.count}
+              </span>
+            </button>
+          </Hint>
         )
       })}
+      <EmojiPopover
+        hint="Add reaction"
+        onEmojiSelect={(emoji) => onReactionChange(emoji.native)}
+      >
+        <button className="flex h-7 items-center rounded-full border border-transparent bg-slate-200/70 px-3 text-slate-800 hover:border-slate-500">
+          <SmilePlus className="size-4" />
+        </button>
+      </EmojiPopover>
     </div>
   )
 }
