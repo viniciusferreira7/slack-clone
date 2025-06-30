387d8e4c51a52dce5679a35f67bf2828683b81f1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-06-30 09:44:48 -0300

    feat: add image adn contact information in profile

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { AlertTriangle, Loader, XIcon } from 'lucide-react'
+import { AlertTriangle, Loader, MailIcon, XIcon } from 'lucide-react'
+import Link from 'next/link'
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { ResizableHandle, ResizablePanel } from '@/components/ui/resizable'
+import { Separator } from '@/components/ui/separator'
 import { usePanel } from '@/hooks/use-panel'
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
 
@@ -28,6 +31,8 @@ export function Profile() {
 
   const showPanel = !isLoading && !!profileMemberId && !parentMessageId
 
+  const avatarFallback = member?.user?.name?.charAt(0).toUpperCase()
+
   return (
     <>
       <ResizableHandle withHandle />
@@ -35,7 +40,7 @@ export function Profile() {
         {!member && !isLoading && (
           <div className="flex h-full flex-col">
             <div className="flex min-h-[49px] items-center justify-between border-b px-4">
-              <p className="text-lg font-bold">Thread</p>
+              <p className="text-lg font-bold">Profile</p>
               <Button
                 variant="ghost"
                 size="iconSm"
@@ -61,6 +66,37 @@ export function Profile() {
               >
                 <XIcon className="size-5 stroke-[1.5]" />
               </Button>
+            </div>
+            <div className="flex flex-col items-center justify-center p-4">
+              <Avatar className="size-full max-h-[256px] max-w-[256px]">
+                <AvatarImage src={member?.user?.image} />
+                <AvatarFallback className="aspect-square bg-sky-500 text-6xl text-white">
+                  {avatarFallback ?? 'M'}
+                </AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="flex flex-col p-4">
+              <p className="text-xl font-bold">{member?.user?.name}</p>
+              <Separator />
+              <div className="flex flex-col p-4">
+                <p className="mb-4 text-sm font-bold">Contact information</p>
+                <div className="flex items-center gap-2">
+                  <div className="flex size-9 items-center justify-center rounded-md bg-muted">
+                    <MailIcon className="size-4" />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[13px] font-semibold text-muted-foreground">
+                      Email address
+                    </p>
+                    <Link
+                      href={`mailto:${member?.user?.email}`}
+                      className="text-sm text-slack-blue-200 hover:underline"
+                    >
+                      {member?.user?.email}
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         ) : (
