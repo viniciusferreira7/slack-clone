b0ff0fd206a03d5ff51f3af6e4467f362e3de944
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-07-03 09:47:24 -0300

    feat: add function to remove member

--- convex/members.ts
+++ convex/members.ts
@@ -2,7 +2,7 @@ import { getAuthUserId } from '@convex-dev/auth/server'
 import { v } from 'convex/values'
 
 import type { Id } from './_generated/dataModel'
-import { query, type QueryCtx } from './_generated/server'
+import { mutation, query, type QueryCtx } from './_generated/server'
 
 const getUserById = async (ctx: QueryCtx, userId: Id<'users'>) => {
   return await ctx.db.get(userId)
@@ -158,3 +158,77 @@ export const update = mutation({
   },
 })
 
+export const remove = mutation({
+  args: {
+    id: v.id('members'),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx)
+    if (!userId) {
+      throw new Error('Unauthorized')
+    }
+
+    const member = await ctx.db.get(args.id)
+
+    if (!member) {
+      throw new Error('Member not found')
+    }
+
+    const currentMember = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', member.workspaceId).eq('userId', userId),
+      )
+      .first()
+
+    if (!currentMember) {
+      throw new Error('Unauthorized')
+    }
+
+    if (member.role === 'admin') {
+      throw new Error('Admin cannot be u')
+    }
+
+    const isSelfTryingToRemove = currentMember._id === member._id
+
+    if (isSelfTryingToRemove && currentMember.role === 'admin') {
+      throw new Error('Cannot remove self is if an admin')
+    }
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query('messages')
+        .withIndex('by_member_id', (q) => q.eq('memberId', member._id))
+        .collect(),
+      ctx.db
+        .query('reactions')
+        .withIndex('by_member_id', (q) => q.eq('memberId', member._id))
+        .collect(),
+      ctx.db
+        .query('conversations')
+        .filter((q) =>
+          q.or(
+            q.eq(q.field('memberOneId'), member._id),
+            q.eq(q.field('memberTwoId'), member._id),
+          ),
+        )
+        .collect(),
+    ])
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id)
+    }
+
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id)
+    }
+
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id)
+    }
+
+    await ctx.db.delete(args.id)
+
+    return { memberId: member._id }
+  },
+})
